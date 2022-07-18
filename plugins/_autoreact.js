0b74977f108529f48434b2a1f52351687ec32a9a
FZfortirex <99706613+FZfortirex@users.noreply.github.com>
2022-07-18 15:26:01 +0700

    Update _autoreact.js

--- plugins/_autoreact.js
+++ plugins/_autoreact.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
     	}
     })	
 }
-handler.customPrefix = /(bile?k|ban?h|ald?i|gp?p|y?|mmk|p?|b(a|i)?c?(o|i)?(t|d)?|wib?u|zulf?a|zai?n)/i
+handler.customPrefix = /(bile?k|ban?h|ald?i|gp?p|y?|ck?p tw?|p?|b(a|i)?c?(o|i)?(t|d)?|wib?u|zulf?a||gali?h||ary?a||aj?i|zai?n)/i
 handler.command = new RegExp
 
 module.exports = handler
